d85ffd4c2048dcdac3da9f2b6e5d6205869cde0e
smouillour <smouillour@talend.com>
2021-02-10 10:50:51 +0100

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import { FacetedSearchIcon } from './FacetedSearchIcon';
 import { FacetedSearch } from './FacetedSearch';
 import { AdvancedSearch } from './AdvancedSearch';
 import { BasicSearch } from './BasicSearch';
-import { BadgeCheckboxes } from './Badges';
+import { BadgeText, BadgeSlider, BadgeOverlay, BadgeCheckboxes } from './Badges';
 import * as badgeDefinitionTypes from './types/badgeDefinition.type';
 
 export {
